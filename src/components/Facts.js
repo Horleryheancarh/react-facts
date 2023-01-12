d93909642da872f5f42d7a2f512d3541bef0d2f7
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-01-12 13:39:16 +0100

    refactored to take more screens

---
+++ src/components/Facts.js
@@ -0,0 +1,13 @@
+import Header from '../components/facts/Header'
+import Footer from '../components/facts/Footer'
+import MainContent from '../components/facts/MainContent'
+
+export default function Fact() {
+	return (
+		<div>
+			<Header />
+			<MainContent />
+			<Footer />
+		</div>
+	)
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+import Fact from "./components/Facts";
 import './index.css'
 
 function App() {
 	return (
 		<div>
-			<Header />
-			<MainContent />
-			<Footer />
+			<Fact />
 		</div>
 	)
 }
